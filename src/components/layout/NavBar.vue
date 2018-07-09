ec4ac7bba9fd18b82fe84e5d913c39c0f52777d6
VVNoodle <brickkace@gmail.com>
2018-07-09 13:21:02 -0700

    Fully pushed the sidenav from Implementation branch!!!!!111

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -175,6 +175,42 @@
     </v-dialog>
   </v-layout>
 
+  <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list class="pa-1">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg" >
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+          @click="console.log('palceholder')"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
 </div>
 </template>
 
@@ -200,7 +236,11 @@
         timestamp: null,
         found_dialog: false,
         lost_dialog: false,
-        drawer: null
+        drawer: null,
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' }
+        ]
       }
     },
     methods:{
