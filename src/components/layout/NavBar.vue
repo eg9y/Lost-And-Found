e245c67e18ba913da3714b2c9e29528680818fa2
VVNoodle <brickkace@gmail.com>
2018-07-11 23:19:08 -0700

    Added userID for each lost and found items. Refactored Navbar

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -39,140 +39,20 @@
 
   <!-- // FOUND ITEM pop up submission form -->
   <v-layout row justify-center>
-    <v-dialog v-model="found_dialog" persistent max-width="500px">
-      <v-card>
-        <v-card-title>
-          <span class="headline">Add Found Item</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container grid-list-md>
-            <v-layout wrap>
-              <v-flex xs12>
-                <v-text-field
-                  v-model="type"
-                  label="Item *"
-                  hint="What did you find?"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field
-                  v-model="description"
-                  label="Item Description"
-                  hint="Please describe the item."
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field
-                  v-model="location"
-                  label="Location Found"
-                  hint="Where did you find the item?"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field
-                  v-model="timestamp"
-                  label="Date Found"
-                  hint="When did you find the item?"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field
-                  v-model="contactEmail"
-                  label="Contact Information"
-                  hint="(E-mail only for now)"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-flex>
-            </v-layout>
-          </v-container>
-          <br />
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click.native="found_dialog = false" @click="addFound">Submit</v-btn>
-          <v-btn color="blue darken-1" flat @click.native="found_dialog = false">Close</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <!-- Seperate Component -->
+    <add-found 
+        :user="user"
+        :foundDialog="found_dialog"
+    ></add-found>
   </v-layout>
 
   <!-- // LOST ITEM pop up submission form -->
   <v-layout row justify-center>
-    <v-dialog v-model="lost_dialog" persistent max-width="500px">
-      <v-card>
-        <v-card-title>
-          <span class="headline">Add Lost Item</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container grid-list-md>
-            <v-layout wrap>
-              <v-flex xs12>
-                <v-text-field
-                  v-model="type"
-                  label="Item *"
-                  hint="What did you lose?"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field
-                  v-model="description"
-                  label="Item Description"
-                  hint="Please describe the item."
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field
-                  v-model="location"
-                  label="Location Found"
-                  hint="Where did you lose the item?"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field
-                  v-model="timestamp"
-                  label="Date Found"
-                  hint="When did you lose the item?"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field
-                  v-model="contactEmail"
-                  label="Contact Information"
-                  hint="(E-mail only for now)"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-flex>
-            </v-layout>
-          </v-container>
-          <br />
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click.native="lost_dialog = false" @click="addLost">Submit</v-btn>
-          <v-btn color="blue darken-1" flat @click.native="lost_dialog = false">Close</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+      <!-- Seperate Component -->
+      <add-lost 
+        :user="user"
+        :lostDialog="lost_dialog"
+      ></add-lost>
   </v-layout>
 
   <v-navigation-drawer
@@ -216,64 +96,52 @@
 
 
 <script>
-  import db from '@/firebase/init'
   import firebase from 'firebase'
   import {mapState} from 'vuex'
+
+  import AddLost from './AddLost/Index'
+  import AddFound from './AddFound/Index'
+
+  import {EventBus} from '../../main';
+
   export default{
     name: 'AddFound',
+    components: {
+      "add-lost": AddLost,
+      "add-found": AddFound
+    },
     computed: {
     ...mapState([
       'isUserLoggedIn',
       'user'
     ])
   },
-    data(){
-      return{
-        type: null,
-        description: null,
-        contactEmail: null,
-        location: null,
-        timestamp: null,
-        found_dialog: false,
-        lost_dialog: false,
-        drawer: null,
-        items: [
-          { title: 'Home', icon: 'dashboard' },
-          { title: 'About', icon: 'question_answer' }
-        ]
+  data(){
+    return{
+      type: null,
+      description: null,
+      contactEmail: null,
+      location: null,
+      timestamp: null,
+      lost_dialog: false,
+      found_dialog: false,
+      drawer: null,
+      items: [
+        { title: 'Home', icon: 'dashboard' },
+        { title: 'About', icon: 'question_answer' }
+      ]
+    }
+  },
+  created () {
+    EventBus.$on('toggleDialog', function (lostOrFound) {
+      if (lostOrFound === 'lost') {
+        this.lost_dialog = false
+      } else {
+        this.found_dialog = false
       }
-    },
+    }.bind(this));
+  },
     methods:{
-      addFound(){
-        if(this.type){
-            this.feedback = null
-            db.collection('found-items').add({
-                type: this.type,
-                description: this.description,
-                contactEmail: this.contactEmail,
-                location: this.location,
-                timestamp: this.timestamp
-            })
-        }
-        else{
-            this.feedback = 'You must enter an item type'
-        }
-      },
-      addLost(){
-          if(this.type){
-              this.feedback = null
-              db.collection('lost-items').add({
-                  type: this.type,
-                  description: this.description,
-                  contactEmail: this.contactEmail,
-                  location: this.location,
-                  timestamp: this.timestamp
-              })
-          }
-          else{
-              this.feedback = 'You must enter an item type'
-          }
-      },
       auth() {
         var provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().useDeviceLanguage();
