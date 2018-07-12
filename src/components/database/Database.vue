901cf573936c65f75f403abd442fcb8e54198c71
Lily Nguyen <nguyen.lily.p@gmail.com>
2018-07-12 12:00:16 -0700

    modified some of the styling for display page

--- src/components/database/Database.vue
+++ src/components/database/Database.vue
@@ -5,13 +5,11 @@
       <div class="card-content">
         <h2 class="indigo-text">Lost: {{ lostItem.type }}</h2>
         <div>
+            <img class = "item-pictures" id="lost-pic" src="" alt="(NO PICTURE AVAILABLE)"><br/>
             Description: {{ lostItem.description }}<br/>
             Contact: {{ lostItem.contactEmail }}<br/>
             Time Stamp: {{ lostItem.timestamp }}<br/>
             Location: {{ lostItem.location }}<br/>
-            <!-- include script here to display picture -->
-            Picture:<br/>
-              <img id="lost-pic" src="" alt="no picture" height="200" width="200"><br/>
         </div>
       </div>
     </div><br/>
@@ -20,12 +18,11 @@
       <div class="cardFound-content">
         <h2 class="indigo-text">Found: {{ foundItem.type }}</h2>
         <div>
+            <img class = "item-pictures" id="lost-pic" src="" alt="(NO PICTURE AVAILABLE)"><br/>
             Description: {{ foundItem.description }}<br/>
             Contact: {{ foundItem.contactEmail }}<br/>
             Time Stamp: {{ foundItem.timestamp }}<br/>
             Location: {{ foundItem.location }}<br/>
-            Picture:<br/>
-              <img id="lost-pic" src="" alt="(NO PICTURE AVAILABLE)" height="200" width="200"><br/>
         </div>
       </div>
     </div>
@@ -96,7 +93,7 @@ export default {
 .index{
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 30px;
+  grid-gap: 50px;
   margin-top: 60px;
 }
 .index h2{
@@ -104,18 +101,11 @@ export default {
   text-align: center;
   margin-top: 0;
 }
-.index .ingredients{
-  margin: 30px auto;
-}
-.index .ingredients li{
-  display: inline-block;
-}
-.index .delete{
-  position: absolute;
-  top: 4px;
-  right: 4px;
-  cursor: pointer;
-  color: #aaa;
-  font-size: 1.4em;
+.item-pictures{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 200px;
+  max-height: 200px;
 }
 </style>
