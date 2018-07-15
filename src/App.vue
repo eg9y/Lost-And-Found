7f753e460fa90f41c59a5296a17d1b42a92c2e7d
unknown <cjiang16@ucsc.edu>
2018-07-14 23:20:39 -0700

    Inprogress

--- src/App.vue
+++ src/App.vue
@@ -8,57 +8,64 @@
 </template>
 
 <script>
-import firebase from 'firebase'
-import Navbar from '@/components/layout/Navbar'
+import firebase from "firebase";
+import Navbar from "@/components/layout/Navbar";
+import { mapState } from 'vuex'
 
 export default {
   components: {
-    'nav-bar': Navbar
+    "nav-bar": Navbar
   },
-  data () {
-    return {
-
-    }
+  data() {
+    return {};
   },
-  created () {
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        // User is signed in.
-        this.fetchAllDocuments()
-        this.$store.dispatch('setUser', user)
-      }
-    }.bind(this))
+  computed: {
+    ...mapState([
+      'db',
+      'user'
+    ])
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(
+      function(user) {
+        if (user) {
+          // User is signed in.
+          this.$store.dispatch("setUser", user);
+          this.fetchAllDocuments();
+        }
+      }.bind(this)
+    );
   },
   methods: {
-    fetchDocuments (collectionName) {
-      let documents = []
-      if (this.user) {
-        this.db.collection(collectionName).where('userID', '==', this.user.uid)
-          .get()
-          .then((items) => {
-            console.log('this :', this)
-            items.forEach((doc) => {
-              // doc.data() is never undefined for query doc snapshots
-              console.log(doc.id, ' => ', doc.data())
-              documents.push(doc.data())
-              if (collectionName === "lost-items") {
-                this.$store.dispatch('setLostItems', documents)
-              } else {
-                this.$store.dispatch('setFoundItems', documents)
-              }
-            })
-          })
-          .catch(function (error) {
-            console.log('Error getting documents: ', error)
-          })
-      }
+    fetchDocuments(collectionName) {
+      let documents = [];
+      this.db
+        .collection(collectionName)
+        .where("userID", "==", this.user.uid)
+        .get()
+        .then(items => {
+          console.log("this :", this);
+          items.forEach(doc => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            documents.push(doc.data());
+          });
+          if (collectionName === "lost-items") {
+            this.$store.dispatch("setLostItems", documents);
+          } else {
+            this.$store.dispatch("setFoundItems", documents);
+          }
+        })
+        .catch(function(error) {
+          console.log("Error getting documents: ", error);
+        });
     },
     // Get all documents by user from lost-items and found-items collection
     // and put it to lost_items and found_items array
-    fetchAllDocuments () {
-      this.fetchDocuments('lost-items')
-      this.fetchDocuments('found-items')
+    fetchAllDocuments() {
+      this.fetchDocuments("lost-items");
+      this.fetchDocuments("found-items");
     }
   }
-}
+};
 </script>
