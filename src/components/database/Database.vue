1092c6ceb21fda406e5115f20de4567a2da1979f
wansfong <wansfong@gmail.com>
2018-07-16 03:09:37 -0700

    Pprototype layout for display items

--- src/components/database/Database.vue
+++ src/components/database/Database.vue
@@ -1,11 +1,58 @@
 <template>
   <div class="index container">
+    <!-- messy but will organize later-->
+    <!--Displays Lost Items-->
+    <div class="card" v-for="lostItem in lostItems" :key="lostItem.id">
+            <v-layout justify-center="20px">
+            <v-flex xs12 sm9 offset-sm>
+            <v-card height="525px">
+            <v-card-title primary-title>
+            <div class="card-content">
+            <div>
+               <img class="item-pictures" v-bind:id="lostItem.id" src="" alt="(NO PICTURE AVAILABLE)"><br/>
+            </div>
+            <h3 class="headline mb-0"><center><b>Lost:</b> {{lostItem.type}}</center></h3>
+            <img class><br><b>Description:</b> {{ lostItem.description }}<br/> <b>Contact:</b> {{ lostItem.contactEmail }}<br/> <b>Time Stamp:</b> {{ lostItem.timestamp }}<br/> <b>Location:</b> {{ lostItem.location }}<br/><br/>
+          </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn bottom flat color="orange">Contact</v-btn>
+            <v-btn bottom flat color="orange">Location</v-btn>
+         </v-card-actions>
+          </v-card>
+          </v-flex>
+          </v-layout>
+        </div><br/>
 
+      <!--Displays Found Items-->
+      <div class="card" v-for="foundItem in foundItems" :key="foundItem.id">
+            <v-layout justify-center="20px">
+            <v-flex xs12 sm9 offset-sm>
+            <v-card height ="500px">
+            <v-card-title primary-title>
+            <div class="card-content">
+            <div>
+               <img class="item-pictures" v-bind:id="foundItem.id" src="" alt="(NO PICTURE AVAILABLE)"><br/>
+            </div>
+            <h3 class="headline mb-0"><center><b>Found:</b> {{foundItem.type}}</center></h3>
+            <img class><br><b>Description:</b> {{ foundItem.description }}<br/> <b>Contact:</b> {{ foundItem.contactEmail }}<br/> <b>Time Stamp:</b> {{ foundItem.timestamp }}<br/> <b>Location:</b> {{ foundItem.location }}<br/><br/>
+          </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn bottom flat color="orange">Contact</v-btn>
+            <v-btn bottom flat color="orange">Location</v-btn>
+         </v-card-actions>
+          </v-card>
+          </v-flex>
+          </v-layout>
+        </div><br/>
+
+    <!--Lily's code
     <div class="card" v-for="lostItem in lostItems" :key="lostItem.id">
       <div class="card-content">
         <h2 class="indigo-text">Lost: {{ lostItem.type }}</h2>
         <div>
-          <img class="item-pictures" v-bind:id="lostItem.id" src="" alt="[ITEM PICTURE]"><br/> Description: {{ lostItem.description }}<br/> Contact: {{ lostItem.contactEmail }}<br/> Time Stamp: {{ lostItem.timestamp }}<br/> Location: {{ lostItem.location }}<br/>
+          <img class="item-pictures" v-bind:id="lostItem.id" src="" alt="(NO PICTURE AVAILABLE)"><br/> Description: {{ lostItem.description }}<br/> Contact: {{ lostItem.contactEmail }}<br/> Time Stamp: {{ lostItem.timestamp }}<br/> Location: {{ lostItem.location }}<br/>
         </div>
       </div>
     </div><br/>
@@ -14,11 +61,10 @@
       <div class="card-content">
         <h2 class="indigo-text">Found: {{ foundItem.type }}</h2>
         <div>
-          <img class="item-pictures" v-bind:id="foundItem.id" src="" alt="[ITEM PICTURE]"><br/> Description: {{ foundItem.description }}<br/> Contact: {{ foundItem.contactEmail }}<br/> Time Stamp: {{ foundItem.timestamp }}<br/> Location: {{ foundItem.location }}<br/>
+          <img class="item-pictures" v-bind:id="foundItem.id" src="" alt="(NO PICTURE AVAILABLE)"><br/> Description: {{ foundItem.description }}<br/> Contact: {{ foundItem.contactEmail }}<br/> Time Stamp: {{ foundItem.timestamp }}<br/> Location: {{ foundItem.location }}<br/>
         </div>
       </div>
-    </div>
-
+    </div>-->
   </div>
 </template>
 
@@ -37,10 +83,12 @@ export default {
     }
   },
   methods: {
-    /* displays all of the "lost" items in the db */
+    /** *  ***/
     displayLost () {
       // fetch data from firestore
-      db.collection('lost-items').get()
+      db
+        .collection('lost-items')
+        .get()
         .then(snapshot => {
           snapshot.forEach(doc => {
             let lostItem = doc.data()
@@ -52,10 +100,12 @@ export default {
           })
         })
     },
-    /* displays all of the "found" items in the db */
+    /** *  ***/
     displayFound () {
       // fetch data from firestore
-      db.collection('found-items').get()
+      db
+        .collection('found-items')
+        .get()
         .then(snapshot => {
           snapshot.forEach(doc => {
             let foundItem = doc.data()
@@ -67,14 +117,16 @@ export default {
           })
         })
     },
-    /* fetches the picture from Storage, url given by urlPic, and replaces the associated img tag src with the url */
+    /** * fetches the picture from Storage, url given by urlPic,
+     *   and replaces the associated img tag src with the url ***/
     getPicture (urlPic, elemID) {
       storage.refFromURL(urlPic).getDownloadURL().then(function (url) {
         var img = document.getElementById(elemID)
         img.src = url
-      }).catch(function (error) {
-        console.log(error)
       })
+        .catch(function (error) {
+          console.log(error)
+        })
     }
   },
   created () {
