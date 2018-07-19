be84986b9516f899c6f8ea195a968ebd12272888
wansfong <wansfong@gmail.com>
2018-07-19 00:27:18 -0700

    organized code in database

--- src/components/database/Database.vue
+++ src/components/database/Database.vue
@@ -1,51 +1,51 @@
 <template>
   <div class="index container">
-    <!-- messy but will organize later-->
-    <!--Displays Lost Items-->
+
+    <!--Sets the design and displays Lost Items-->
     <div class="card" v-for="lostItem in lostItems" :key="lostItem.id">
-            <v-layout justify-center="20px">
-            <v-flex xs12 sm9 offset-sm>
-            <v-card height="525px">
+      <v-layout justify-center="20px">
+        <v-flex xs12 sm9 offset-sm>
+          <v-card height="525px">
             <v-card-title primary-title>
-            <div class="card-content">
-            <div>
-               <img class="item-pictures" v-bind:id="lostItem.id" src="" alt="(NO PICTURE AVAILABLE)"><br/>
-            </div>
-            <h3 class="headline mb-0"><center><b>Lost:</b> {{lostItem.type}}</center></h3>
-            <img class><br><b>Description:</b> {{ lostItem.description }}<br/> <b>Contact:</b> {{ lostItem.contactEmail }}<br/> <b>Time Stamp:</b> {{ lostItem.timestamp }}<br/> <b>Location:</b> {{ lostItem.location }}<br/><br/>
-          </div>
-          </v-card-title>
+              <div class="card-content">
+                <div>
+                  <img class="item-pictures" v-bind:id="lostItem.id" src="" alt="(NO PICTURE AVAILABLE)"><br/>
+                </div>
+                <h3 class="headline mb-0"><center><b>Lost:</b> {{lostItem.type}}</center></h3>
+                <img class><br><b>Description:</b> {{ lostItem.description }}<br/> <b>Contact:</b> {{ lostItem.contactEmail }}<br/> <b>Time Stamp:</b> {{ lostItem.timestamp }}<br/> <b>Location:</b> {{ lostItem.location }}<br/><br/>
+              </div>
+            </v-card-title>
           <v-card-actions>
             <v-btn bottom flat color="orange">Contact</v-btn>
             <v-btn bottom flat color="orange">Location</v-btn>
-         </v-card-actions>
+          </v-card-actions>
           </v-card>
-          </v-flex>
-          </v-layout>
-        </div><br/>
+        </v-flex>
+      </v-layout>
+    </div><br/>
 
       <!--Displays Found Items-->
       <div class="card" v-for="foundItem in foundItems" :key="foundItem.id">
-            <v-layout justify-center="20px">
-            <v-flex xs12 sm9 offset-sm>
+        <v-layout justify-center="20px">
+          <v-flex xs12 sm9 offset-sm>
             <v-card height ="500px">
-            <v-card-title primary-title>
-            <div class="card-content">
-            <div>
-               <img class="item-pictures" v-bind:id="foundItem.id" src="" alt="(NO PICTURE AVAILABLE)"><br/>
-            </div>
-            <h3 class="headline mb-0"><center><b>Found:</b> {{foundItem.type}}</center></h3>
-            <img class><br><b>Description:</b> {{ foundItem.description }}<br/> <b>Contact:</b> {{ foundItem.contactEmail }}<br/> <b>Time Stamp:</b> {{ foundItem.timestamp }}<br/> <b>Location:</b> {{ foundItem.location }}<br/><br/>
-          </div>
-          </v-card-title>
-          <v-card-actions>
-            <v-btn bottom flat color="orange">Contact</v-btn>
-            <v-btn bottom flat color="orange">Location</v-btn>
-         </v-card-actions>
-          </v-card>
+              <v-card-title primary-title>
+                <div class="card-content">
+                  <div>
+                    <img class="item-pictures" v-bind:id="foundItem.id" src="" alt="(NO PICTURE AVAILABLE)"><br/>
+                  </div>
+                  <h3 class="headline mb-0"><center><b>Found:</b> {{foundItem.type}}</center></h3>
+                  <img class><br><b>Description:</b> {{ foundItem.description }}<br/> <b>Contact:</b> {{ foundItem.contactEmail }}<br/> <b>Time Stamp:</b> {{ foundItem.timestamp }}<br/> <b>Location:</b> {{ foundItem.location }}<br/><br/>
+                </div>
+              </v-card-title>
+            <v-card-actions>
+              <v-btn bottom flat color="orange">Contact</v-btn>
+              <v-btn bottom flat color="orange">Location</v-btn>
+            </v-card-actions>
+            </v-card>
           </v-flex>
-          </v-layout>
-        </div><br/>
+        </v-layout>
+      </div><br/>
 
     <!--Lily's code
     <div class="card" v-for="lostItem in lostItems" :key="lostItem.id">
