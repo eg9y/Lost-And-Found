12b020c1a70bf5a2c3de9ec8c4fc092bb4f80106
Lily Nguyen <nguyen.lily.p@gmail.com>
2018-07-21 16:03:36 -0700

    updating changes

--- test/unit/specs/GMap.spec.js
+++ test/unit/specs/GMap.spec.js
@@ -1,24 +1,13 @@
-import Vue from 'vue'
-// import Index from '@/components/home/SubmissionForm/Index'
+import Vue from 'Vue'
 import GMap from '@/components/home/GMap'
 
 describe('GMap.vue', () => {
-  it('cheks that the submission form starts closed', () => {
-    // const IndexConstructor = Vue.extend(Index)
+  it('prevents markers being added outside of UCSC boundaries', () => {
     const Constructor = Vue.extend(GMap)
-    // const IndexComponent = new IndexConstructor().$mount()
     const GMapComponent = new Constructor().$mount()
-    // console.log('**********')
-    // console.log(GMapComponent)
-    // console.log(IndexComponent.submissionDialog)
-    expect(GMapComponent.submissionDialog).to.equal(false)
-  })
 
-  it('prevents markers being created outside the boundaries of UCSC', () => {
-    const Constructor = Vue.extend(GMap)
-    const GMapComponent = new Constructor().$mount()
+    // const button = ListComponent.$el.querySelector('button');
     console.log('**********')
-    // console.log(GMapComponent)
-    console.log(GMapComponent.$parent.getMap)
+    console.log(GMapComponent.$el.querySelector('.text-xs-center'))
   })
 })
