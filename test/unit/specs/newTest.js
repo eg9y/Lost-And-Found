36c4a920b9858ea419aab9764c204c31ec0643bd
wansfong <wansfong@gmail.com>
2018-07-21 22:39:46 -0700

    code commit

--- test/unit/specs/newTest.js
+++ test/unit/specs/newTest.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import database from '@/components/database/database'
 
+var assert = chai.assert
+
 describe('Database.vue', () => {
   it('should display the results from the database', function () {
+    assert.equal(arr.length, 0)
     const Constructor = Vue.extend(database)
     const databaseComponent = new Constructor().$mount()
     expect(databaseComponent.submissionDialog).to.equal(true)
