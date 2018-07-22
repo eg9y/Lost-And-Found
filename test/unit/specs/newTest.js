afa6e6d64ef06eb1d1aeaa2da4732f08288de80f
wansfong <wansfong@gmail.com>
2018-07-21 22:09:04 -0700

    new test

--- test/unit/specs/newTest.js
+++ test/unit/specs/newTest.js
@@ -1,11 +1,10 @@
-// import Vue from 'vue'
-// import App from '@/App'
+import Vue from 'vue'
+import database from '@/components/database/database'
 
-var assert = require('assert')
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1, 2, 3].indexOf(4))
-    })
+describe('Database.vue', () => {
+  it('should display the results from the database', function () {
+    const Constructor = Vue.extend(database)
+    const databaseComponent = new Constructor().$mount()
+    expect(databaseComponent.submissionDialog).to.equal(true)
   })
 })
