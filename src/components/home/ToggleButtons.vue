6a527466fae6eb75e27291ff589f1e36d6e5e257
GeoffHuang <ghuang6@ucsc.edu>
2018-07-22 17:01:12 -0700

    idea: floating toolbar for buttons??

--- src/components/home/ToggleButtons.vue
+++ src/components/home/ToggleButtons.vue
@@ -1,25 +1,18 @@
 <template>
-<!-- idea: wrap this in a v dialog that's always true, and then put this component inside gmapmap -->
-  <!-- <v-card flat class="py-5">
-    <v-card-text>
-      <v-container fluid class="pa-0">
-        <v-layout row wrap> -->
-          <v-flex xs12 sm6 class="py-2">
-            <v-btn-toggle v-model="toggle_multiple" multiple>
-              <v-btn @click="displayLost" value="Lost">
-                Lost
-              </v-btn>
-              <v-btn @click="displayFound" value="Found">
-                Found
-              </v-btn>
-            </v-btn-toggle>
-          </v-flex>
-        <!-- </v-layout>
-      </v-container>
-    </v-card-text>
-  </v-card> -->
+  <!-- <v-toolbar dense floating> -->
+    <v-flex xs12 sm6 class="py-2">
+      <v-btn-toggle v-model="toggle_multiple" multiple>
+        <v-btn @click="displayLost" value="Lost">
+          Lost
+        </v-btn>
+        <v-btn @click="displayFound" value="Found">
+          Found
+        </v-btn>
+      </v-btn-toggle>
+    </v-flex>
+  <!-- </v-toolbar> -->
 
-<!-- wrapped in v-dialog makes it appear over the map, but makes the map faded -->
+   <!-- wrapped in v-dialog makes it appear over the map, but makes the map faded -->
   <!-- <v-dialog v-model="toggleButtons" persistent max-width="112px" lazy>
     <v-btn-toggle v-model="toggle_multiple" multiple>
       <v-btn @click="displayLost" value="Lost">
