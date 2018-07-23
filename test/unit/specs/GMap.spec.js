90f4e8d19dae202ae167e2974835dc56fd588804
Lily Nguyen <nguyen.lily.p@gmail.com>
2018-07-23 14:32:31 -0700

    added a unit test

--- test/unit/specs/GMap.spec.js
+++ test/unit/specs/GMap.spec.js
@@ -2,6 +2,12 @@ import Vue from 'Vue'
 import GMap from '@/components/home/GMap'
 
 describe('GMap.vue', () => {
+  it('checks that the submission form is closed, when component is first opened', () => {
+    const Constructor = Vue.extend(GMap)
+    const GMapComponent = new Constructor().$mount()
+    expect(GMapComponent.submissionDialog).to.equal(false)
+  })
+
   it('prevents markers being added outside of UCSC boundaries', () => {
     const Constructor = Vue.extend(GMap)
     const GMapComponent = new Constructor().$mount()
