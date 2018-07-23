53258815023cd414725c919e8269b0fa16a9423c
wansfong <wansfong@gmail.com>
2018-07-23 15:14:19 -0700

    user location implementation

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -3,6 +3,7 @@
     <v-alert icon="new_releases" style="margin=0 0 0 0;" v-model="alert" dismissible type="error" transition="slide-y-transition">
       You must log in to pin!
     </v-alert>
+    <v-btn @click="findMarker('SA9diJQJeMmRJbQkzAMZ')">Hi</v-btn>
     <GmapMap :center="center" :zoom="16" :options="mapOptions" style="width: 100%; height: 100%" ref="mapRef" @dragend="checkBoundary" @click="addLocation">
       <submission-form :lat="lat" :lng="lng" :submissionDialog="submissionDialog" :user="user"></submission-form>
       <gmap-info-window
@@ -17,35 +18,17 @@
         </v-layout>
         <v-layout>
           <v-flex v-if="infoWinOpen" transition="fade">
-              <h3>{{infoWindow.description}}</h3>
-              <h3>{{infoWindow.timestamp}}</h3>
-              <h3>{{infoWindow.contactEmail | truncate}}</h3>
+              <h2>{{infoWindow.description}}</h2>
+              <h2>{{infoWindow.timestamp}}</h2>
+              <h2>{{infoWindow.contactEmail | truncate}}</h2>
           </v-flex>
         </v-layout>
         <div class="text-xs-center">
           <v-btn v-if="isUserLoggedIn && user.uid == infoWindow.userID" @click="deleteMarker" color="error">Resolve</v-btn>
         </div>
       </gmap-info-window>
-      <GmapMarker
-        v-if="all_lost_items"
-        :animation="2"
-        v-for="(lost_item, index) in all_lost_items"
-        :key="`lost-${index}-${lost_item.location._lat},${lost_item.location._long}`"
-        :position="{lat: lost_item.location._lat, lng: lost_item.location._long}"
-        :title="lost_item.type"
-        :clickable="true"
-        icon="../../../static/icons/lost_icon.png"
-        @click="getMarkerDetails(lost_item, 'Lost: ', 'lost-items')" />
-      <GmapMarker
-        v-if="all_found_items"
-        :animation="2"
-        v-for="(found_item, index) in all_found_items"
-        :key="`found-${index}-${found_item.location._lat},${found_item.location._long}`"
-        :position="{lat: found_item.location._lat, lng: found_item.location._long}"
-        :title="found_item.type"
-        :clickable="true"
-        icon="../../../static/icons/found_icon.png"
-        @click="getMarkerDetails(found_item, 'Found: ', 'found-items')" />
+      <GmapMarker v-if="all_lost_items" :animation="2" v-for="(lost_item, index) in all_lost_items" :key="`lost-${index}-${lost_item.location._lat},${lost_item.location._long}`" :position="{lat: lost_item.location._lat, lng: lost_item.location._long}" :title="lost_item.type" :clickable="true" icon="../../../static/icons/lost_icon.png" @click="getMarkerDetails(lost_item, index, 'Lost: ', 'lost-items')" />
+      <GmapMarker v-if="all_found_items" :animation="2" v-for="(found_item, index) in all_found_items" :key="`found-${index}-${found_item.location._lat},${found_item.location._long}`" :position="{lat: found_item.location._lat, lng: found_item.location._long}" :title="found_item.type" :clickable="true" icon="../../../static/icons/found_icon.png" @click="getMarkerDetails(found_item, index, 'Found: ', 'found-items')" />
 
       <GmapMarker
         v-if="lat && lng"
@@ -62,19 +45,15 @@ import { gmapApi } from 'vue2-google-maps'
 import SubmissionForm from './SubmissionForm/Index'
 import { EventBus } from '../../main'
 import { mapState } from 'vuex'
-import firebase from 'firebase'
-
 // these coordinates define the boundaries of the map/UCSC
 const MIN_LAT = 36.987615
 const MAX_LAT = 37.001976
 const MIN_LNG = -122.068846
 const MAX_LNG = -122.04808
-
 export default {
   components: {
     'submission-form': SubmissionForm
   },
-  name: 'gmap',
   data () {
     return {
       // lat and lng are used for location
@@ -88,15 +67,13 @@ export default {
         pictures: null,
         location: null,
         contactEmail: null,
-        // timestamp: null,
-        date: null,
-        time: null,
+        timestamp: null,
         userID: null,
         id: null,
         collectionName: null
       },
       infoWinOpen: false,
-      currentMid: null,
+      currentMidx: null,
       // optional: offset infowindow so it visually sits nicely on top of our marker
       infoOptions: {
         pixelOffset: {
@@ -117,9 +94,6 @@ export default {
     }
   },
   methods: {
-    /*
-
-    */
     checkBoundary () {
       var strictBounds = new this.google.maps.LatLngBounds(
         new this.google.maps.LatLng(MIN_LAT, MIN_LNG),
@@ -127,22 +101,18 @@ export default {
       )
       this.$refs.mapRef.$mapPromise.then(map => {
         if (strictBounds.contains(map.getCenter())) return
-
         // We're out of bounds - Move the map back within the bounds
         let c = map.getCenter()
         let x = c.lng()
         let y = c.lat()
-
         let maxX = strictBounds.getNorthEast().lng()
         let maxY = strictBounds.getNorthEast().lat()
         let minX = strictBounds.getSouthWest().lng()
         let minY = strictBounds.getSouthWest().lat()
-
         if (x < minX) x = minX
         if (x > maxX) x = maxX
         if (y < minY) y = minY
         if (y > maxY) y = maxY
-
         map.setCenter(new this.google.maps.LatLng(y, x))
       })
     },
@@ -150,7 +120,7 @@ export default {
       Closes the currently open info window, assigns values from selected marker to info window, opens the info window
       Parameters: ???
     */
-    getMarkerDetails (marker, collectionTitle, collectionName) {
+    getMarkerDetails (marker, idx, collectionTitle, collectionName) {
       this.closeInfoWindow()
       if (marker.location) {
         // wait 1/4 seconds to set new info for info window
@@ -160,26 +130,36 @@ export default {
           this.infoWindow.type = collectionTitle + marker.type
           this.infoWindow.description = marker.description
           this.infoWindow.contactEmail = marker.contactEmail
-          // this.infoWindow.timestamp = marker.timestamp
-          this.infoWindow.date = marker.date
-          this.infoWindow.time = marker.time
+          this.infoWindow.timestamp = marker.timestamp
           this.infoWindow.userID = marker.userID
           this.infoWindow.collectionName = collectionName
           this.infoWindow.id = marker.id
           console.log('Info Window ID: ' + this.infoWindow.id)
-
           // check if its the same marker that was selected if yes toggle
-          if (this.currentMid === marker.id) {
+          if (this.currentMidx === idx) {
             this.infoWinOpen = !this.infoWinOpen
           } else { // if different marker set infowindow to open and reset current marker index
             this.infoWinOpen = true
-            this.currentMid = marker.id
+            this.currentMidx = idx
           }
-          console.log(this.infoWinOpen)
-          console.log(this.currentMid)
         }, 400)
       }
     },
+
+    // Wan edit: made geolocation function
+    geolocation () {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+        this.addLocation({
+          lat: this.center.lat,
+          lng: this.center.lng
+        })
+      })
+    },
+
     /*
       Updates the location for a new potential marker, and opens the submission form
       Parameters: e -- event object from clicking the map
@@ -198,13 +178,10 @@ export default {
       // open the submission form
       this.submissionDialog = true
     },
-    /*
-      Deletes the marker's associated entry in the db, and deletes the picture from Storage if applicable
-    */
     deleteMarker () {
       // deletes associated picture if item has one, and it's stored in Storage
       if (this.infoWindow.pictures && this.infoWindow.pictures.includes(this.infoWindow.userID)) {
-        var picRef = firebase.storage().refFromURL(this.infoWindow.pictures)
+        var picRef = this.firebase.storage().refFromURL(this.infoWindow.pictures)
         picRef.delete().then(function () {
           console.log('Image successfully deleted from Storage')
         // eslint-disable-next-line
@@ -212,7 +189,6 @@ export default {
           console.log('Error in deleting image from Storage')
         })
       }
-
       // deletes the entry from the db and then updates the local copies
       this.db.collection(this.infoWindow.collectionName).doc(this.infoWindow.id).delete().then(() => {
         this.$store.dispatch('updateUserCollection', this.infoWindow.collectionName)
@@ -223,9 +199,6 @@ export default {
         console.error('Error removing document: ', error)
       })
     },
-    /*
-
-    */
     closeInfoWindow () {
       this.infoWinOpen = false
       this.infoWindow.type = null
@@ -238,30 +211,15 @@ export default {
       this.infoWindow.id = null
       this.infoWindow.collectionName = null
     },
-    findMarker (itemStr) {
-      console.log('findMarker is running, looking for: ' + itemStr)
-      console.log(typeof itemStr)
-      if (this.all_lost_items && this.all_found_items) {
-        var itemID = itemStr.substr(2)
-        if (itemStr[0] === 'l') {
-          for (var i = 0; i < this.all_lost_items.length; i++) {
-          // console.log('ddd', this.all_lost_items[i])
-            if (this.all_lost_items[i].id === itemID) {
-              console.log('it\'s a match')
-              console.log(this.all_lost_items[i].id)
-              this.getMarkerDetails(this.all_lost_items[i], 'Lost: ', 'lost-items')
-              i = this.all_lost_items.length
-            }
-          }
-        } else if (itemStr[0] === 'f') {
-          for (var j = 0; j < this.all_found_items.length; j++) {
-          // console.log('ddd', this.all_lost_items[i])
-            if (this.all_found_items[j].id === itemID) {
-              console.log('it\'s a match')
-              console.log(this.all_found_items[j].id)
-              this.getMarkerDetails(this.all_found_items[j], 'Found: ', 'found-items')
-              j = this.all_Found_items.length
-            }
+    findMarker (itemID) {
+      console.log('findMarker is running, looking for: ' + itemID)
+      if (this.all_lost_items) {
+        for (var i = 0; i < this.all_lost_items.length; i++) {
+          if (this.all_lost_items[i].id === itemID) {
+            console.log('it\'s a match')
+            console.log(this.all_lost_items[i].id)
+            this.getMarkerDetails(this.all_lost_items[i], i, 'Lost: ', 'lost-items')
+            i = this.all_lost_items.length
           }
         }
       }
@@ -280,26 +238,18 @@ export default {
     ])
   },
   created () {
+    console.log(this.$route.params.id)
     EventBus.$on('toggleSubmission', function (submission) {
       this.submissionDialog = false
-      this.lat = null
-      this.lng = null
+      // this.lat = null
+      // this.lng = null
+      this.geolocation()
     }.bind(this))
-    if (this.$route.params.id) {
-      this.findMarker(this.$route.params.id)
-    }
   },
-  watch: {
-    all_lost_items () {
-      if (this.$route.params.id) {
-        this.findMarker(this.$route.params.id)
-      }
-    },
-    all_found_items () {
-      if (this.$route.params.id) {
-        this.findMarker(this.$route.params.id)
-      }
-    }
+  mounted () {
+    EventBus.$on('locateItem', function (itemID) {
+      this.findMarker(itemID)
+    }.bind(this))
   },
   filters: {
     // Define truncate filter to replace long words with ...
