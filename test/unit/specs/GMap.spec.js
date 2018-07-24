373e120f803bc1ff3afdae19ac0ba76556418231
Geoff Huang <ghuang6@ucsc.edu>
2018-07-23 18:01:05 -0700

    Lily's unit test

--- test/unit/specs/GMap.spec.js
+++ test/unit/specs/GMap.spec.js
@@ -3,11 +3,15 @@ import GMap from '@/components/home/GMap'
 
 describe('GMap.vue', () => {
   it('checks that the submission is closed when component is first loaded', () => {
+    const Constructor = Vue.extend(GMap)
+    const GMapComponent = new Constructor().$mount()
+    expect(GMapComponent.submissionDialog).to.equal(false)
   })
-  it('prevents markers being added outside of UCSC boundaries', () => {
+  // Following test is not working
+  /* it('prevents markers being added outside of UCSC boundaries', () => {
     const Constructor = Vue.extend(GMap)
     const GMapComponent = new Constructor().$mount()
     console.log('**********')
-    console.log('What is this? ', GMapComponent.$el)
-  })
+    console.log(GMapComponent.$el)
+  }) */
 })
