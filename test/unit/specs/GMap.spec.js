4cf42c6cbffb4756accc69872c769fd96862c412
Lily Nguyen <nguyen.lily.p@gmail.com>
2018-07-24 21:26:42 -0700

    Update GMap.spec.js
    fixed typo in GMap unit test

--- test/unit/specs/GMap.spec.js
+++ test/unit/specs/GMap.spec.js
@@ -2,17 +2,7 @@ import Vue from 'Vue'
 import GMap from '@/components/home/GMap'
 
 describe('GMap.vue', () => {
-<<<<<<< HEAD
   it('checks that the submission form is closed, when component is first opened', () => {
-    const Constructor = Vue.extend(GMap)
-    const GMapComponent = new Constructor().$mount()
-    expect(GMapComponent.submissionDialog).to.equal(false)
-  })
-
-  it('prevents markers being added outside of UCSC boundaries', () => {
-=======
-  it('checks that the submission is closed when component is first loaded', () => {
->>>>>>> origin/BACKUP-MASTER
     const Constructor = Vue.extend(GMap)
     const GMapComponent = new Constructor().$mount()
     expect(GMapComponent.submissionDialog).to.equal(false)
